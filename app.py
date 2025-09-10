abf2581a5cc10c6fc556168e0efdafd99e13e40a
Tejas-00 <tejassonawane1709@gmail.com>
2025-09-10 23:31:13 +0530

    done with app.py

--- app.py
+++ app.py
@@ -19,3 +19,48 @@
 
 # order of X: Age->Gender->Tenure->MonthlyCharges
 
+import streamlit as st
+import joblib
+import numpy as np
+
+scaler = joblib.load("scaler.pkl")
+model = joblib.load("model.pkl")
+
+st.title("Churn Prediction")
+
+st.divider()
+
+st.write("please enter the value and hit the predict button to get predict")
+
+st.divider()
+
+age = st.number_input("Enter age: ", min_value=10, max_value=100, value=30)
+
+gender = st.selectbox("Enter the Gender: ", ["Male", "Female"])
+
+tenure = st.number_input("Enter Tenure", min_value=0, max_value=130, value=10)
+
+monthlycharge = st.number_input("Enter Monthly Charge: ", min_value=30, max_value=150)
+    
+
+st.divider()
+
+predictbutton = st.button("Predict")
+
+if predictbutton:
+    gender_selected = 1 if gender == "Female" else 0
+
+    X = [age, gender_selected, tenure, monthlycharge]
+
+    X1 = np.array(X)
+
+    X_array = scaler.transform([X1])
+
+    prediction = model.predict(X_array)
+
+    predicted = "Yes, Churn" if prediction == 1 else "No, Not Churn"
+
+    st.write(f"Predicted: {predicted}")
+
+else:
+    st.write("Please the the values and then use predict button")
