29eb0e7059a88a6f5de5b23200881d148a07ef83
Tejas-00 <tejassonawane1709@gmail.com>
2025-09-10 16:01:42 +0530

    Built and tuned a K-Nearest Neighbors (KNN) and SVM model using GridSearchCV.

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 # Gender -> 1:Female 0:Male
 # Churn -> 1:Yes 0:No
+
+# What is StandardScaler?
+# A tool from scikit-learn used for feature scaling.
+# It standardizes numerical data so that:
+# mean = 0 standard deviation = 1
+# Many machine learning algorithms (like logistic regression, SVM, KNN, neural networks) work better when features are on the same scale.
+# Prevents features with large values (e.g., salary in lakhs) from dominating small ones (e.g., age).
+
+# scaler.fit_transform(X_train)
+# fit() → calculates the mean and standard deviation of each column in X_train.
+# transform() → uses those values to scale the data (so mean = 0, std = 1).
+
+# joblib is a Python library used for saving and loading Python objects efficiently.
+# joblib.dump(object, filename) → saves (serializes) a Python object to a file.
